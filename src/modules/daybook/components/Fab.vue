8a2c4b74f080e757522dc53c6d890c857f8a9a57
aspio28 <aspiolea28@gmail.com>
2024-05-15 15:42:10 -0400

    add vuex

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="btn btn-primary">
+        <div>
+            {{ text }}
+        </div>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                type: String,
+                default: "Agregar"
+            }
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+button {
+    border-radius: 100%;
+    bottom: 20px;
+    height: 80px;
+    position: fixed;
+    right: 20px;
+    width: 80px;
+}
+</style>
